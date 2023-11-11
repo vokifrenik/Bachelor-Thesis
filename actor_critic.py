49e04e8b0e81973c30debaf6ede61a61eb96cf84
vokifrenik <vikplaysmc@gmail.com>
2023-11-11 15:08:39 +0100

    changes to architecture to fix matrix multiplication error

--- actor_critic.py
+++ actor_critic.py
@@ -1,8 +1,9 @@
-import numpy as np
-import torch as T
+# model.py
+
 import torch.nn as nn
-import torch.functional as F
-import torch.optim as optim 
+import torch.nn.functional as F
+import torch.optim as optim
+import torch as T
 
 class GeneralNetwork(nn.Module):
     def __init__(self, lr, input_dims, fc1_dims, fc2_dims, output_dims):
@@ -13,19 +14,18 @@ class GeneralNetwork(nn.Module):
         self.fc2_dims = fc2_dims
         self.output_dims = output_dims
 
-        # Modify the convolutional layers to accept 3D input
+        # Convolutional layers with pooling
         self.conv1 = nn.Conv2d(input_dims[0], fc1_dims, kernel_size=3)
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
         self.conv2 = nn.Conv2d(fc1_dims, fc2_dims, kernel_size=5)
-        
-        # Calculate the size of the linear layer based on the input dimensions
-        linear_input_size = self._calculate_linear_input_size(input_dims)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
 
         # Modify the fully connected layers based on output_dims
         if output_dims == 2:
-            self.fc1 = nn.Linear(linear_input_size, fc1_dims)
+            self.fc1 = nn.Linear(57 * 61 * fc2_dims, fc1_dims)
             self.fc2 = nn.Linear(fc1_dims, output_dims)
         elif output_dims == 1:
-            self.fc1 = nn.Linear(linear_input_size, fc2_dims)
+            self.fc1 = nn.Linear(57 * 61 * fc2_dims, fc2_dims)
             self.fc2 = nn.Linear(fc2_dims, output_dims)
 
         self.optimizer = optim.Adam(self.parameters(), lr=self.lr)
@@ -33,38 +33,33 @@ class GeneralNetwork(nn.Module):
         self.to(self.device)
 
     def forward(self, x):
-        x = nn.functional.relu(self.conv1(x))
-        x = nn.functional.relu(self.conv2(x))
-        x = x.view(x.size(0), -1)  # Flatten the output from conv layers
-        x = nn.functional.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
-
-    def _calculate_linear_input_size(self, input_dims):
-        dummy_input = T.zeros(1, *input_dims)
-        x = nn.functional.relu(self.conv1(dummy_input))
-        x = nn.functional.relu(self.conv2(x))
-        return x.view(1, -1).size(1)
-
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool2(x)
+         # Flatten the output from conv layers
+        x = x.view(x.size(0), -1)  
+       
+        x_fc1 = F.relu(self.fc1(x))
+        x_fc2 = self.fc2(x_fc1)
+        return x_fc2
+    
 
 class Agent(object):
-    def __init__(self, alpha, beta, input_dims, gamma = 0.95, n_actions=7, layer1_size=64, layer2_size=64, n_outputs=2):
+    def __init__(self, alpha, beta, input_dims, gamma=0.95, n_actions=7, layer1_size=64, layer2_size=64):
         self.input_dims = input_dims
         self.log_probs = None
         self.gamma = gamma
-        self.n_outputs = n_outputs
         self.actor = GeneralNetwork(alpha, input_dims, layer1_size, layer2_size, output_dims=2)
         self.critic = GeneralNetwork(beta, input_dims, layer1_size, layer2_size, output_dims=1)
 
     def choose_action(self, state):
-        mu,sigma = self.actor.forward(state)
+        mu, sigma = self.actor.forward(state)
         sigma = T.exp(sigma)
         action_probs = T.distributions.Normal(mu, sigma)
-        probs = action_probs.sample(sample_shape=T.Size([self.n_outputs]))  
+        probs = action_probs.sample(sample_shape=T.Size([self.actor.output_dims]))  
         self.log_probs = action_probs.log_probs(probs).to(self.actor.device)               
-
         return probs.item()
-    
 
     def learn(self, state, reward, n_state, done):
         self.actor.optimizer.zero_grad()
@@ -74,14 +69,12 @@ class Agent(object):
         state_value = self.critic.forward(state)
 
         reward = T.tensor(reward, dtype=float).to(self.actor.device)
-        delta = reward + self.gamma*n_state_value*(1-int(done)) - state_value
+        delta = reward + self.gamma * n_state_value * (1 - int(done)) - state_value
 
-        actor_loss = -self.log_probs*delta
-        critic_loss = delta**2
+        actor_loss = -self.log_probs * delta
+        critic_loss = delta ** 2
 
         (actor_loss + critic_loss).backward()
 
         self.actor.optimizer.step()
         self.critic.optimizer.step()
-
-
--- environment.py
+++ environment.py
@@ -1,14 +1,13 @@
+# environment.py
+
 import numpy as np
-import gym_super_mario_bros
 import torch as T
-from model import Agent
+import gym_super_mario_bros
 from nes_py.wrappers import JoypadSpace
 from gym.wrappers import GrayScaleObservation
 from gym.wrappers import FrameStack
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-
-
-
+from actor_critic import Agent
 
 # Setup environment
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
@@ -19,11 +18,23 @@ env = GrayScaleObservation(env, keep_dim=False)
 env = FrameStack(env, 4)
 
 state = env.reset()
-state = state.__array__()
+
+# Convert the state to a single NumPy array
+state = np.array(state)
+
+# Convert the NumPy array to a PyTorch tensor
 state = T.tensor(state, dtype=T.float32).to('cpu')
+state = state.unsqueeze(0)
+input_tuple = (state,)
+input_tensor = T.stack(input_tuple)
 
+# Get the dimensions of the state space
+input_dims = input_tensor.size()[2:]
 
-agent = Agent(0.000005, 0.000001, input_dims=state.shape, gamma=0.99, n_actions=7, layer1_size=64, layer2_size=64, n_outputs=2)
+print(input_dims)
+
+# Create the Agent with the correct input_dims
+agent = Agent(0.000005, 0.000001, input_dims=input_dims, gamma=0.99, n_actions=7, layer1_size=64, layer2_size=64)
 
 score_history = []
 num_episodes = 100
@@ -33,17 +44,26 @@ for i in range(num_episodes):
     score = 0
     state = env.reset()
 
-    # convert state to tensor
-    state = state.__array__()
+    # Convert state to a single NumPy array
+    state = np.array(state)
+
+    # Convert the NumPy array to a PyTorch tensor
     state = T.tensor(state, dtype=T.float32).to(agent.actor.device)
 
     while not done:
         action = agent.choose_action(state)
         next_state, reward, done, info = env.step(action)
+
+        # Convert next_state to a single NumPy array
+        next_state = np.array(next_state)
+
+        # Convert the NumPy array to a PyTorch tensor
+        next_state = T.tensor(next_state, dtype=T.float32).to(agent.actor.device)
+
         score += reward
 
         agent.learn(state, reward, next_state, done)
         state = next_state
+
     score_history.append(score)
     print('episode ', i, 'score %.2f' % score, '100 game average %.2f' % np.mean(score_history[-100:]))
-

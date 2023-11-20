04bcf8ba77db400d2ac1859a886b3781ef151a4a
vokifrenik <vikplaysmc@gmail.com>
2023-11-20 13:30:53 +0100

    update

--- actor_critic.py
+++ actor_critic.py
@@ -7,9 +7,10 @@ import torch as T
 import random as rand
 import numpy as np
 import matplotlib.pyplot as plt
+from torch.nn import init
 
 class GeneralNetwork(nn.Module):
-   def __init__(self, lr, input_dims, fc1_dims, fc2_dims, output_dims):
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, output_dims):
         super(GeneralNetwork, self).__init__()
 
         self.lr = lr
@@ -26,48 +27,56 @@ class GeneralNetwork(nn.Module):
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(kernel_size=2)
 
-        # Modify the fully connected layers based on output_dims
-        if output_dims == 2:
-            self.fc = nn.Linear(3477, fc1_dims)
-            self.relu_fc = nn.ReLU()
-            self.mu_layer = nn.Linear(fc1_dims, 1)
-            self.log_sigma_layer = nn.Linear(fc1_dims, 1)
-        elif output_dims == 1:
-            self.fc = nn.Linear(3477, fc2_dims)
-            self.relu_fc = nn.ReLU()
-            self.mu_layer = nn.Linear(fc2_dims, 1)
-            self.log_sigma_layer = nn.Linear(fc2_dims, 1)
+        # Common fully connected layer
+        ## Freeze this layer and then update the heads separaetely and vice versa
+        self.fc = nn.Linear(3477, fc1_dims)
+        self.relu_fc = nn.ReLU()
+
+        # Actor head for mean and variance
+        self.mu_layer = nn.Linear(fc1_dims, 1)
+        self.log_sigma_layer = nn.Linear(fc1_dims, 1)  ## update 
+
+        # Critic head for value function
+        self.value_layer = nn.Linear(fc1_dims, 1)
+
 
         self.optimizer = optim.Adam(self.parameters(), lr=self.lr)
         self.device = 'cpu'
         self.to(self.device)
 
-   def forward_actor(self, x):
+        self.apply(self.init_weights)
+
+    def init_weights(self, m):
+        if type(m) == nn.Linear or type(m) == nn.Conv2d:
+            init.xavier_uniform_(m.weight)
+            m.bias.data.fill_(0.01)
+
+    def forward_actor(self, x):
         x = self.relu1(self.conv1(x))
         x = self.pool1(x)
         x = self.relu2(self.conv2(x))
         x = self.pool2(x)
         x = x.view(x.size(0), -1)
         x_fc = self.relu_fc(self.fc(x))
+        ## maybe add another relu after output
         mu = self.mu_layer(x_fc).squeeze(-1)
         log_sigma = self.log_sigma_layer(x_fc).squeeze(-1)
 
         return mu, log_sigma
 
-   def forward_critic(self, x):
+    def forward_critic(self, x):
         x = self.relu1(self.conv1(x))
         x = self.pool1(x)
         x = self.relu2(self.conv2(x))
         x = self.pool2(x)
         x = x.view(x.size(0), -1)
         x_fc = self.relu_fc(self.fc(x))
-        value = self.mu_layer(x_fc).squeeze(-1)
-        #print("value", value)
+        value = self.value_layer(x_fc).squeeze(-1)
 
         return value
     
 class Agent(object):
-    def __init__(self, alpha, beta, input_dims, gamma=0.95, epsilon=0.3, n_actions=7, layer1_size=64, layer2_size=64):
+    def __init__(self, alpha, beta, input_dims, gamma=0.99, epsilon=0.2, n_actions=7, layer1_size=64, layer2_size=64):
         self.input_dims = input_dims
         self.log_probs = None
         self.gamma = gamma
@@ -76,16 +85,22 @@ class Agent(object):
         self.actor = GeneralNetwork(alpha, input_dims, layer1_size, layer2_size, output_dims=2)
         self.critic = GeneralNetwork(beta, input_dims, layer1_size, layer2_size, output_dims=1)
 
+
     def choose_action(self, state):
         mu, log_sigma = self.actor.forward_actor(state)
         sigma = T.exp(log_sigma)
+        #sigma = T.abs(sigma)
+        sigma = T.clamp(sigma, min=1e-6)  # Add a small epsilon to avoid zero
+        
+        #print("mu", mu)
+        #print("sigma", sigma)
 
         # Sample from the normal distribution
         action_probs = T.distributions.Normal(mu, sigma)
 
         # Plot the distribution
-        #plt.plot(action_probs.sample().numpy())
-        #plt.show()
+        plt.plot(action_probs.sample().numpy())
+        plt.show()
 
         # Sample only once and let the distribution broadcast across the batch dimension
         sampled_actions = action_probs.sample()
@@ -120,16 +135,18 @@ class Agent(object):
         
         actor_loss = -self.log_probs * delta
         critic_loss = delta ** 2
+        print("actor_loss", actor_loss)
+        print("critic_loss", critic_loss)
 
-        total_loss = actor_loss.mean() + critic_loss.mean()
+        total_loss = actor_loss.mean() + critic_loss.mean()  ## check this implementation
         print("total_loss", total_loss)
 
         # Perform the backward pass on the scalar total_loss
         total_loss.backward()
 
         # Clip gradients to prevent exploding gradients
-        T.nn.utils.clip_grad_norm_(self.actor.parameters(), max_norm=1.0)
-        T.nn.utils.clip_grad_norm_(self.critic.parameters(), max_norm=1.0)
+        T.nn.utils.clip_grad_norm_(self.actor.parameters(), max_norm=0.5)
+        T.nn.utils.clip_grad_norm_(self.critic.parameters(), max_norm=0.5)
 
         self.actor.optimizer.step()
         self.critic.optimizer.step()
